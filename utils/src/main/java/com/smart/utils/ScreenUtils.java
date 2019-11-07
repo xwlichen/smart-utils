6ef77cbb639fad03e67721a36f1224b0eaedee28
lichen <1960003945@qq.com>
2019-11-07 17:05:15 +0800

    初步提交

---
+++ utils/src/main/java/com/smart/utils/ScreenUtils.java
@@ -0,0 +1,10 @@
+package com.smart.utils;
+
+/**
+ * @date : 2019-11-07 16:30
+ * @author: lichen
+ * @email : 1960003945@qq.com
+ * @description :
+ */
+public class ScreenUtils {
+}
---
+++ utils/src/main/java/com/smart/utils/constants/Constants.java
@@ -0,0 +1,10 @@
+package com.smart.utils.constants;
+
+/**
+ * @date : 2019-11-07 16:27
+ * @author: lichen
+ * @email : 1960003945@qq.com
+ * @description :
+ */
+public class Constants {
+}
